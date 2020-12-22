84f2d851cc05dbe4555d6b3aeb7c677fe5d8c54e
hpazk <hpazk.zkh@gmail.com>
2020-12-22 17:36:43 +0700

    change var to let in modules/fuzzy.mjs

--- modules/fuzzy.mjs
+++ modules/fuzzy.mjs
@@ -4,7 +4,7 @@ export const matchString = (source, text) => {
     let search_pos = 0;
 
     for (let i = 0; i < source.length; i++) {
-        var char = source[i];
+        let char = source[i];
 
         if (search_pos < search.length && char.toLowerCase() == search[search_pos]) {
             char = char;
